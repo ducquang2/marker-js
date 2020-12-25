d939ca80c3b35672a1c5543469bd227fb201ddba
Khanh <tran_khanh@outlook.com>
2020-12-25 23:14:34 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,37 @@
 module.exports = {
     future: {
-      removeDeprecatedGapUtilities: true,
+        removeDeprecatedGapUtilities: true,
     },
     experimental: {
-      uniformColorPalette: true,
-      extendedFontSizeScale: true,
-      applyComplexClasses: true,
+        uniformColorPalette: true,
+        extendedFontSizeScale: true,
+        applyComplexClasses: true,
     },
     purge: {
-      content: ['./src/index.html', './src/**/*.js'],
+        content: ['./src/index.html', './src/**/*.js'],
     },
     theme: {
-      interFontFeatures: {
-        default: ['calt', 'liga', 'kern'],
-        numeric: ['tnum', 'salt', 'ss02'],
-      },
-      extend: {},
+        interFontFeatures: {
+            default: ['calt', 'liga', 'kern'],
+            numeric: ['tnum', 'salt', 'ss02'],
+        },
+        extend: {},
+        screens: {
+            'tablet': '640px',
+            // => @media (min-width: 640px) { ... }
+
+            'laptop': '1024px',
+            // => @media (min-width: 1024px) { ... }
+
+            'desktop': '1280px',
+            // => @media (min-width: 1280px) { ... }
+        },
     },
     variants: {},
     plugins: [
-      require('tailwindcss-font-inter')({
-        importFontFace: true,
-        disableUnusedFeatures: true,
-      }),
+        require('tailwindcss-font-inter')({
+            importFontFace: true,
+            disableUnusedFeatures: true,
+        }),
     ],
-  };
+};
